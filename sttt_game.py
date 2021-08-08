17d9f225d7517578506b3c410ee4ebbe45b63f6c
Andrew Chen <andrew.lang.chen@gmail.com>
2021-08-08 00:29:58 -0400

    fixed documentation

--- sttt_game.py
+++ sttt_game.py
@@ -10,40 +10,40 @@ class sttt_game:
         self.next_allowed = 0  # the box that the next player can play in (see 'game state')
         self.turn = 1  # the current turn (decides which player is active and what game state is returned)
 
-    """
-    'game_state' returns 2 args: 
-    - the current game board (9x9 array of ints)
-        - 0 = open
-        - 1 = taken by the opponent
-        - 2 = taken by the current player
-    - the allowed space for the next move (int)
-        - 0 = the last move was in a complete 3x3 square / the player can play anywhere [0-8, 0-8]
-        - 1 = any coordinate within [0-2, 0-2]
-        - 2 = any coordinate within [0-2, 3-5]
-        - 3 = any coordinate within [0-2, 6-8]
-        - 4 = any coordinate within [3-5, 0-2]
-        - 5 = any coordinate within [3-5, 3-5]
-        - 6 = any coordinate within [3-5, 6-8]
-        - 7 = any coordinate within [6-8, 0-2]
-        - 8 = any coordinate within [6-8, 3-5]
-        - 9 = any coordinate within [6-8, 6-8]
-    """
     def game_state(self):
+        """
+        'game_state' returns a list containing 2 args:
+        - [0] the current game board (9x9 array of ints)
+            - 0 = open
+            - 1 = taken by the opponent
+            - 2 = taken by the current player
+        - [1] the allowed space for the next move (int)
+            - 0 = the last move was in a complete 3x3 square / the player can play anywhere [0-8, 0-8]
+            - 1 = any coordinate within [0-2, 0-2]
+            - 2 = any coordinate within [0-2, 3-5]
+            - 3 = any coordinate within [0-2, 6-8]
+            - 4 = any coordinate within [3-5, 0-2]
+            - 5 = any coordinate within [3-5, 3-5]
+            - 6 = any coordinate within [3-5, 6-8]
+            - 7 = any coordinate within [6-8, 0-2]
+            - 8 = any coordinate within [6-8, 3-5]
+            - 9 = any coordinate within [6-8, 6-8]
+        """
         pass
 
-    """
-    'move' checks that a move is valid, executes if valid, and returns True/False accordingly
-    if not valid, one of these error messages will be printed:
-    - "Error: Not a move - {bot name} executed move {the move} on turn {the turn}" 
-        - No proper coordinates were given
-    - "Error: Illegal move - {bot name} executed move {the move} on turn {the turn}"
-        - A coordinate was given but it did not fit the allowed ranges as specified by game_state
-    """
     def move(self):
+        """
+        'move' checks that a move is valid, executes if valid, and returns True/False accordingly
+        if not valid, one of these error messages will be printed:
+        - "Error: Not a move - {bot name} executed move {the move} on turn {the turn}"
+            - No proper coordinates were given
+        - "Error: Illegal move - {bot name} executed move {the move} on turn {the turn}"
+            - A coordinate was given but it did not fit the allowed ranges as specified by game_state
+        """
         pass
 
-    """
-    'is_finished' returns a boolean - True if the game is finished, False if not
-    """
     def is_finished(self):
+        """
+        'is_finished' returns a boolean - True if the game is finished, False if not
+        """
         return self.finished
